f89d23e201bbb8580075a4eaec576bd7b71394df
vanzsyah <zuhdiansyahivan@gmail.com>
2025-02-17 00:51:47 +0700

    Update app.py

--- App/app.py
+++ App/app.py
@@ -3,15 +3,10 @@ import streamlit as st
 import pandas as pd
 
 # Load saved models and data
-#df_clustered, dbi_score, cluster_counts, cluster_eval_results = joblib.load('../src/model_data.pkl')
 
-try:
-    with open("../src/model_data.pkl", "rb") as f:
+with open("../src/model_data.pkl", "rb") as f:
         df_clustered, dbi_score, cluster_counts, cluster_eval_results = joblib.load(f)
-    print("File loaded successfully!")
-except Exception as e:
-    print(f"Error loading file: {e}")
-
+   
 # Generate recommendations
 def generate_recommendations(model, user_id, items_to_predict):
     recommendations = [(item, model.predict(user_id, item).est) for item in items_to_predict]
